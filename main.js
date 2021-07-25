e81e000264aba3bd37d3bf22f68a9a2aba2b814e
ejm218 <67883042+ejm218@users.noreply.github.com>
2021-07-25 11:51:20 -0400

    still no idea whats wrong with text wrapping

--- main.js
+++ main.js
@@ -98,12 +98,22 @@ let areaCv = document.getElementById('area-cv')
 
 let resumeButton = document.getElementById('resume-button')
 
+let opt = {
+  margin:       0,
+  filename:     'Merritt_CV.pdf',
+  image:        { type: 'jpeg', quality: 0.98 },
+  html2canvas:  { scale: 4 },
+  jsPDF:        { format: 'a4', orientation: 'portrait' }
+};
+
 function generateResume(){
-  html2pdf(areaCv)
+  html2pdf(areaCv, opt)
 }
 
 resumeButton.addEventListener('click', () =>{
   scaleCv()
 
   generateResume()
+
+  setTimeout(removeScale, 5000)
 })
